634bc07122f31e98f4ea2864029c48b2deccbe78
data0gyus <dwjsk2008@gmail.com>
2025-06-19 17:16:48 +0900

    06-19 수업

---
+++ aopex/src/main/java/org/scoula/config/RootConfig.java
@@ -0,0 +1,14 @@
+package org.scoula.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration          // Spring 설정 클래스임을 선언
+@ComponentScan(basePackages = {
+        "org.scoula.advice",           // AOP Advice 패키지 스캔
+        "org.scoula.sample.service"    // 비즈니스 서비스 패키지 스캔
+})
+@EnableAspectJAutoProxy    // AspectJ Auto Proxy 활성화 (핵심!)
+public class RootConfig {
+}
